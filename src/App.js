31a93a12c4d52026f75fe504946df87d5f587386
Akamo Dolapo <dolapoakamo01@gmail.com>
2023-10-28 12:38:58 +0900

    update

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useCallback } from 'react'
+import React from 'react'
 import { useState, useEffect } from "react";
 import FavoritesList from "./FavouriteList";
 import CityList from "./CityList";
